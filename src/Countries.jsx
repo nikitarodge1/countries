a0c708774da06103ca1a0f737cbe9c2457d052f1
Your Name <you@example.com>
2024-05-19 18:37:52 +0530

    countrycard

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 const Tile = ({ FlagUrl, countryName, altFlagName }) => (
   <div
+    className="countryCard" 
     style={{
       display: "flex",
       justifyContent: "center",
@@ -22,7 +23,7 @@ const Tile = ({ FlagUrl, countryName, altFlagName }) => (
 function Countries() {
   const [countries, setCountries] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-  const [error, setError] = useState(null); 
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -33,7 +34,10 @@ function Countries() {
         return response.json();
       })
       .then((data) => setCountries(data))
-      .catch((error) => setError(error.message)); 
+      .catch((error) => {
+        console.error("Error occurred:", error); 
+        setError(error.message);
+      });
   }, []);
 
   const filteredCountries = countries.filter(country =>
@@ -48,11 +52,11 @@ function Countries() {
         placeholder="Search for a country..."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        style={{ marginBottom: "10px", padding: "8px", fontSize: "16px" }}
+        style={{ marginBottom: "10px", padding: "8px", fontSize: "16px",border: "1px solid grey", display: "block", margin: "0 auto", width: "60%",borderRadius:"5px" }} 
       />
-      {error && <p style={{ color: "red" }}>{error}</p>} 
+      {error && <p style={{ color: "red", textAlign: "center" }}>{error}</p>}
       <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", gap: "10px" }}>
-        {filteredCountries.map((country) => (
+        {filteredCountries.length > 0 && filteredCountries.map((country) => (
           <Tile
             key={country.cca3}
             FlagUrl={country.flags.png}
